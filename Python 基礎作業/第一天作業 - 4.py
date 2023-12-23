53769d66f842401c4a22d30e89ce579ea67faa5d
Allen Chen <mhhao17@gmail.com>
2023-12-23 16:34:00 +0800

    first day homework

--- Python 基礎作業/第一天作業 - 4.py
+++ Python 基礎作業/第一天作業 - 4.py
@@ -5,3 +5,4 @@ secondary_num = random.randint(1, 254)
 third_num = random.randint(1, 254)
 four_num = random.randint(1, 254)
 print(f'IPv4地址是: {first_num}.{secondary_num}.{third_num}.{four_num}')
+test
