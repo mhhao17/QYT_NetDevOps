4c01eea67dee46ff40b629ef40cb7013b60b6231
Allen Chen <mhhao17@gmail.com>
2023-12-24 15:41:44 +0800

    first day homework

--- Python 基礎作業/Random_IPv4.py
+++ Python 基礎作業/Random_IPv4.py
@@ -1,7 +1,7 @@
 import random
 
-first_num = random.randint(1, 254)
-secondary_num = random.randint(1, 254)
-third_num = random.randint(1, 254)
-four_num = random.randint(1, 254)
-print(f'IPv4地址是: {first_num}.{secondary_num}.{third_num}.{four_num}')
+a = random.randint(1, 254)
+b = random.randint(1, 254)
+c = random.randint(1, 254)
+d = random.randint(1, 254)
+print(f'IPv4地址是: {a}.{b}.{c}.{d}')
