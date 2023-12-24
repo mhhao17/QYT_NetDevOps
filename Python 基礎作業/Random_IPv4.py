05de6f13b4bad1a11c9c3d078eb94e2dd8dfc01d
Allen Chen <mhhao17@gmail.com>
2023-12-25 05:20:07 +0800

    add comment

--- Python 基礎作業/Random_IPv4.py
+++ Python 基礎作業/Random_IPv4.py
@@ -1,7 +1,10 @@
 import random
 
+# 定義四個變數，分別代表四個IP位址的數字, 範圍是1~254
 a = random.randint(1, 254)
 b = random.randint(1, 254)
 c = random.randint(1, 254)
 d = random.randint(1, 254)
+
+# 顯示四個變數的值
 print(f'IPv4地址是: {a}.{b}.{c}.{d}')
