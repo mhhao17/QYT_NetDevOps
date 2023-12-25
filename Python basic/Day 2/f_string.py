aa82da61c8c2ecd774fee28825683455b4fc5424
Allen Chen <mhhao17@gmail.com>
2023-12-25 10:59:24 +0800

    Correct the word

--- Python basic/Day 2/f_string.py
+++ Python basic/Day 2/f_string.py
@@ -5,10 +5,10 @@ depart2_m = 'qinke'
 COURSE_FEES_SEC = 456789.12456
 COURSE_FEES_Python = 1234.3456
 
-line1 = 'Dearptment1 name:%-10s  Manager:%-10s  COURSE FEES:%-10.2f  The End!' % (department1, depart1_m, COURSE_FEES_SEC)
-line2 = 'Dearptment2 name:%-10s  Manager:%-10s  COURSE FEES:%-10.2f  The End!' % (department2, depart2_m, COURSE_FEES_Python)
-# line1 = ('Dearptment1 name:{0:<10}  Manager:{1:<10}  COURSE FEES:{2:<10.2f}  The End!'.format(department1, depart1_m, COURSE_FEES_SEC))
-# line2 = ('Dearptment2 name:{0:<10}  Manager:{1:<10}  COURSE FEES:{2:<10.2f}  The End!'.format(department2, depart2_m, COURSE_FEES_Python))
+# line1 = 'Department1 name:%-10s  Manager:%-10s  COURSE FEES:%-10.2f  The End!' % (department1, depart1_m, COURSE_FEES_SEC)
+# line2 = 'Department2 name:%-10s  Manager:%-10s  COURSE FEES:%-10.2f  The End!' % (department2, depart2_m, COURSE_FEES_Python)
+line1 = ('Department1 name:{0:<10}  Manager:{1:<10}  COURSE FEES:{2:<10.2f}  The End!'.format(department1, depart1_m, COURSE_FEES_SEC))
+line2 = ('Department2 name:{0:<10}  Manager:{1:<10}  COURSE FEES:{2:<10.2f}  The End!'.format(department2, depart2_m, COURSE_FEES_Python))
 
 length = len(line1)
 print('=' * length)
