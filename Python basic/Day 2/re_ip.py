8c7f0c254574ecb2d9e5b1ea6ea8a572de3cc3b6
Allen Chen <mhhao17@gmail.com>
2023-12-25 14:02:46 +0800

    Day 2 homework - 5

---
+++ Python basic/Day 2/re_ip.py
@@ -0,0 +1,10 @@
+import re
+
+str1 = 'Port-channel1.189          192.168.189.254  YES     CONFIG   up'
+
+result = re.match(r'\s*(\w.*\d)\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(\w*)\s+(\w*)\s+(\w*)\s+(\w*)\s*', str1).groups()
+
+print('-'*70)
+print('%-6s : %s' % ('接口', result[0]))
+print('%-6s : %s' % ('IP地址', result[1]))
+print('%-6s : %s' % ('狀態', result[-1]))
