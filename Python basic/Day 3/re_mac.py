0a0e2600850bcf3360fc5855dad036daabdb73a8
Allen Chen <mhhao17@gmail.com>
2023-12-26 12:35:46 +0800

    第三天作業，第一題

---
+++ Python basic/Day 3/re_mac.py
@@ -0,0 +1,10 @@
+import re
+
+str1 = '166 54a2.74f7.0326 DYNAMIC Gi1/0/11'
+
+result = re.match(r'(\d+)\s+([0-9a-fa-f]{4}\.[0-9a-fa-f]{4}\.[0-9a-fa-f]{4})\s+(\w+)\s+(\S+)', str1).groups()
+
+print('%-10s : %s' % ('VLAN ID', result[0]))
+print('%-10s : %s' % ('MAC', result[1]))
+print('%-10s : %s' % ('TYPE', result[-2]))
+print('%-10s : %s' % ('Interface', result[-1]))
