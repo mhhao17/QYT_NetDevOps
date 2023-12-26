1ba941a97127a10390d51d46611417f664cffc76
Allen Chen <mhhao17@gmail.com>
2023-12-27 04:45:40 +0800

    優化正規表示式去匹配MAC

--- Python basic/Day 3/re_mac.py
+++ Python basic/Day 3/re_mac.py
@@ -2,7 +2,7 @@ import re
 
 str1 = '166 54a2.74f7.0326 DYNAMIC Gi1/0/11'
 
-result = re.match(r'(\d+)\s+([0-9a-fa-f]{4}\.[0-9a-fa-f]{4}\.[0-9a-fa-f]{4})\s+(\w+)\s+(\S+)', str1).groups()
+result = re.match(r'(\d+)\s+([0-9a-f]{4}\.[0-9a-f]{4}\.[0-9a-f]{4})\s+(\w+)\s+(\S+)', str1).groups()
 
 print('%-10s : %s' % ('VLAN ID', result[0]))
 print('%-10s : %s' % ('MAC', result[1]))
