8d1502733cb73e8471be9f5e13f9741488767acd
Allen Chen <mhhao17@gmail.com>
2023-12-31 01:45:40 +0800

    add comment

--- Python basic/Day 2/Create_new_word.py
+++ Python basic/Day 2/Create_new_word.py
@@ -1,5 +1,17 @@
+'''
+功能 : 輸入一個單字並重新自創一個新單字
+'''
+
 word = input("請輸入一個單字：")
+
+# 將所輸入單字的index 0 放置變數 first_letter
 first_letter = word[0]
+
+# 將index 1之後的字串放置變數 sub_word
 sub_word = word[1:]
+
+# 重新合併字串
 new_word = sub_word + '-' + first_letter + 'y'
+
+# 輸出合併完成的字串 new_word
 print(new_word)
