95a4242238cfc1cfd80d77ba11b62989feba6a86
Allen Chen <mhhao17@gmail.com>
2023-12-31 03:21:46 +0800

    fix re

--- Python basic/Day 2/re_ip.py
+++ Python basic/Day 2/re_ip.py
@@ -2,7 +2,8 @@ import re
 
 str1 = 'Port-channel1.189          192.168.189.254  YES     CONFIG   up'
 
-result = re.match(r'\s*(\w.*\d)\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(\w*)\s+(\w*)\s+(\w*)\s+(\w*)\s*', str1).groups()
+result = re.match(r'(\w.*\d)\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(\w*)\s+(\w*)\s+(\w*)',
+                  str1).groups()
 
 print('-'*70)
 print('%-6s : %s' % ('接口', result[0]))
