bc7a3ce615fff0c8f25691c03d4427c6afacc75d
Allen Chen <mhhao17@gmail.com>
2024-01-11 07:50:15 +0800

    adjust time parameter

--- Python basic/Day 14/save_time_to_file.py
+++ Python basic/Day 14/save_time_to_file.py
@@ -2,7 +2,7 @@ import datetime
 
 current_time = datetime.datetime.now()
 fiveDaysAgo = current_time - datetime.timedelta(days=5)
-formatted_time = current_time.strftime('%Y-%m-%d_%H-%M-%S')
+formatted_time = current_time.strftime('%F_%H-%M-%S')
 
 file_name = f"save_fivedayago_time_{formatted_time}.txt"
 context_to_write = f"{fiveDaysAgo}"
